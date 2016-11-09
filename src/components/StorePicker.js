8a857e3a653236140e6c7dbcac03a94249cffc9a
Steven Fitzpatrick <steven.fitzpatrick1985@gmail.com>
2016-11-09 20:46:27 +0000

    Routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import { getFunName}  from '../helpers';
 
 class StorePicker extends React.Component{
+	/*constructor(){
+		super();
+		this.goToStore = this.goToStore.bind(this);
+	}*/
+
+	goToStore(event){
+		event.preventDefault();
+		const input = this.storeInput.value;
+		this.context.router.transitionTo(`/store/${input}`);
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={ e => this.goToStore(e) }>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name" 
+				defaultValue={getFunName()} 
+				ref={(input) => {this.storeInput = input}} />
 				<button type="submit">Visit Store</button>
 			</form>	
 		)
 		
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
